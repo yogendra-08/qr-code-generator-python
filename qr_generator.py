b7de40fb90f4952022e57a360ed2b2d2538ae634
yogendra-08 <yogendradbhange@gmail.com>
2025-12-30 07:56:55 +0530

    add check url is valid or not && save generated file with custom name taking input from user

--- qr_generator.py
+++ qr_generator.py
@@ -1,11 +1,21 @@
 import qrcode
 
 #taking url as input from user 
-
 url = input("Enter the URL to generate QR code: ")
 
+#taking filename as input from user
+filename = input("Enter the filename to save the QR code (with .png extension): ")
+
+#check the url is valid or not
+if not url.startswith("http://") and not url.startswith("https://"):
+    print("Invalid URL. Please enter a valid URL.")
+    exit()
+
+#generating qr code
 img = qrcode.make(url)
 
-img.save("qr_code.png")
+#saving the qr code as png file
+img.save(f"{filename}".png)
 
+#displaying the message
 print("QR code generated and saved as qr_code.png")
